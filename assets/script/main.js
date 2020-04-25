a3dcddfcc0546c6c6b1c10f5d92a1585c4ac341f
ElderSlayer <dideath1@gmail.com>
2020-04-26 00:51:13 +0300

    add class game

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,53 +1,92 @@
 "use strict";
 
-class Game {
-	amount;
-}
-
 class Card {
 
-	name;
+	value;
 	url;
 	status = 'close';
 
 }
 
-let amount = document.querySelector('#amount').value;
+class Game {
+	amountHtml = document.querySelector('#amount');
+	amount = document.querySelector('#amount').value;
+	cards = [];
 
-let cards = [];
+	/*constructor() {
+		this.initializeEvents();
+	}*/
 
-for (let i = 0; i < amount; i++) {
-	cards.push(new Card);
-}
+	getRandomInteger(min, max) {
+		let rand = Math.random() * (max + 1 - min) + min;
+		return Math.floor(rand);
+	}
 
-function randomInteger(min, max) {
-	let rand = Math.random() * (max + 1 - min) + min;
-	return Math.floor(rand);
-}
+	getRandomNumberCard(arr, amount, key) {
+		let max = amount / 2;
+		let ranNum;
+		ranNum = this.getRandomInteger(1, max);
 
-function ranNumTwoRepeat(arr, amount, key) {
-	let max = amount / 2;
-	let ranNum;
-	ranNum = randomInteger(1, max);
-
-	for ( let i = 0; i < amount; i++ ) {
-		if ( arr[i][key] === ranNum ) {
-			for (let j = 0; j < amount; j++) {
-				if (arr[j][key] === ranNum && i !== j) {
-					return ranNumTwoRepeat(arr, amount, key);
+		for ( let i = 0; i < amount; i++ ) {
+			if ( arr[i][key] === ranNum ) {
+				for (let j = 0; j < amount; j++) {
+					if (arr[j][key] === ranNum && i !== j) {
+						return this.getRandomNumberCard(arr, amount, key);
+					}
 				}
+				return  ranNum;
 			}
-			return  ranNum;
+		}
+		return ranNum;
+	}
+
+	createCards() {
+		for (let i = 0; i < this.amount; i++) {
+			this.cards.push(new Card);
 		}
 	}
-	return ranNum;
+
+	addNameUrlCards() {
+		this.cards.forEach( (e)=> {
+			e.value = this.getRandomNumberCard(this.cards, this.amount, 'value');
+			e.url = `assets/img/card-${e.value}.png`;
+		})
+	}
+
+	addHtmlCards() {
+		let field = document.querySelector('#field');
+
+		this.cards.forEach( (e)=> {
+			let cardHtml = document.createElement('div');
+			let imgCardBackHtml = document.createElement('img');
+			let imgCardMainHtml = document.createElement('img');
+
+			imgCardBackHtml.src = 'assets/img/card-back.png';
+			imgCardBackHtml.className = 'card__back';
+
+			imgCardMainHtml.src = e.url;
+			imgCardMainHtml.className = 'card__img';
+
+			cardHtml.className = 'card';
+			cardHtml.append(imgCardBackHtml, imgCardMainHtml);
+
+			field.append(cardHtml);
+		})
+	}
+
+	startGame() {
+		this.createCards();
+		this.addNameUrlCards();
+		this.addHtmlCards();
+		console.log(this);
+	}
+
+	/*initializeEvents() {
+		this.amountHtml.addEventListener('change', () => {
+			console.log('test');
+		})
+	}*/
 }
 
-cards.forEach(function (e) {
-	e.name = ranNumTwoRepeat(cards, amount, 'name');
-	e.url = `assets/img/card-${e.name}`;
-})
-
-console.log(cards);
-
-
+let newGame = new Game;
+newGame.startGame();
